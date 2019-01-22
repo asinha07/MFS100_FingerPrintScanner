ead6b50bd1cef4f21c887e1ba6d91346cf4085df
Aman Sinha <aman.sinha@quikr.com>
2019-01-22 10:29:17 +0530

    Updated Starter package

--- src/android/MFS100_FingerPrintScanner.java
+++ src/android/MFS100_FingerPrintScanner.java
@@ -2,31 +2,26 @@ package cordova-plugin-mfs100;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONException;
 
-/**
- * This class echoes a string called from JavaScript.
- */
 public class MFS100_FingerPrintScanner extends CordovaPlugin {
-
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+        if ("echo".equals(action)) {
+            echo(args.getString(0), callbackContext);
             return true;
         }
+
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
+    private void echo(String msg, CallbackContext callbackContext) {
+        if (msg == null || msg.length() == 0) {
+            callbackContext.error("Empty message!");
         } else {
-            callbackContext.error("Expected one non-empty string argument.");
+            callbackContext.success(msg);
         }
     }
 }
--- www/MFS100_FingerPrintScanner.js
+++ www/MFS100_FingerPrintScanner.js
@@ -1,5 +1,18 @@
-var exec = require('cordova/exec');
+var cordova = require('cordova');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'MFS100_FingerPrintScanner', 'coolMethod', [arg0]);
+var echo = function(arg0, success, error) {
+    cordova.exec(success, error, "ModusEcho", "echo", [arg0]);
 };
+
+var echojs = function(arg0, success, error) {
+    if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
+        console.log(arg0);
+    } else {
+        error('Empty message!');
+    }
+};
+
+var failed = function() {
+	console.log("failed");
+}
+echo("Hi",echojs,failed);
